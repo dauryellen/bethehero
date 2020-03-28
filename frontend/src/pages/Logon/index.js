935f530909cbaab2dfd5cc532c0d16d7d2df53ee
Daury Mendes <dauryellen@live.com>
2020-03-28 18:34:43 -0300

    conexão frontend com backend da aplicação

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import './styles.css';
 
@@ -8,15 +10,37 @@ import heroesImg from '../../assets/heroes.png';
 import logoImg from '../../assets/logo.svg';
 
 export default function Logon(){
+  const [id, setId] = useState('');
+  const history = useHistory();
+
+  async function handleLogon(event) {
+    event.preventDefault();
+
+    try {
+      const response = await api.post('session', { id });
+
+      localStorage.setItem('ongId', id);
+      localStorage.setItem('ongName', response.data.name);
+
+      history.push('/profile');
+    } catch (error) {
+      alert("Falha no login. Tente novamente.");
+    }
+  }
+
   return (
     <div className="logon-container">
       <section className="form">
         <img src={logoImg} alt="Be The Hero"/>
 
-        <form>
+        <form onSubmit={handleLogon}>
           <h1>Faça seu logon</h1>
 
-          <input placeholder="Sua ID" />
+          <input 
+            placeholder="Sua ID" 
+            value={id}
+            onChange={event => setId(event.target.value)}
+          />
           <button className="button" type="submit" >Entrar</button>
 
           <Link className="back-link" to="/register">
--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,12 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState} from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import './styles.css';
 
 import logoImg from '../../assets/logo.svg';
 
 export default function NewIncident(){
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [value, setValue] = useState('');
+
+  const ongId = localStorage.getItem('ongId');
+
+  const history = useHistory();
+
+  async function handleNewIncident(event){
+    event.preventDefault();
+
+    const data = {
+      title,
+      description,
+      value,
+    };
+
+    try {
+      await api.post('incidents', data, {
+        headers: {
+          Authorization: ongId,
+        }
+      }); 
+
+      history.push('/profile');
+    } catch (error) {
+      alert('Erro ao cadastrar caso. Tente novamente.');
+    }
+  }
+
   return (
     <div className="new-incident-container">
       <div className="content">
@@ -24,11 +56,22 @@ export default function NewIncident(){
             Voltar para home
           </Link>
         </section>
-        <form>
-          <input placeholder="Título do caso" />
-          <textarea placeholder="Descrição" />
-          <input placeholder="Valor em reais" />    
-
+        <form onSubmit={handleNewIncident}>
+          <input 
+            placeholder="Título do caso"
+            value={title}
+            onChange={event => setTitle(event.target.value)}
+          />
+          <textarea 
+            placeholder="Descrição"
+            value={description}
+            onChange={event => setDescription(event.target.value)}
+          />
+          <input 
+            placeholder="Valor em reais"
+            value={value}
+            onChange={event => setValue(event.target.value)}
+          />    
           <button className="button" type="submit">Cadastrar</button>
         </form>
       </div>
